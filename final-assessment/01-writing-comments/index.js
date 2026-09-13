c01d8f3aa0b9963c3d2fc3385d1c3f2100ca0879
Devin <devindes03@gmail.com>
2026-09-14 05:00:26 +0800

    Initial commit

---
+++ final-assessment/01-writing-comments/index.js
@@ -0,0 +1,7 @@
+// 2305551173_devin
+
+/*
+Goal tahun ini:
+1. Belajar JavaScript.
+2. Menjadi Front-End atau Back-End Developer.
+*/
---
+++ my-personal-notes/src/components/NotesList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import NoteItem from './NoteItem';
+
+function NotesList({ notes, onDelete, onArchive, dataTestId = 'notes-list' }) {
+  const hasNotes = notes && notes.length > 0;
+
+  if (!hasNotes) {
+    return (
+      <div className="notes-list" data-testid={dataTestId}>
+        <p
+          className="notes-list__empty-message"
+          data-testid={`${dataTestId}-empty`}
+        >
+          Tidak ada catatan
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="notes-list" data-testid={dataTestId}>
+      {notes.map((note) => (
+        <NoteItem
+          key={note.id}
+          note={note}
+          onDelete={onDelete}
+          onArchive={onArchive}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default NotesList;
